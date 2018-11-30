e24582fd18686a71c3a1fd04aa6f3ffc6f5a1a0b
justin garcia <justinframedev@gmail.com>
2018-11-30 14:04:37 -0700

    layout start

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(".dropdown-trigger").dropdown();
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
