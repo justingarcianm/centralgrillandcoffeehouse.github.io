01cd1fd6afa96492cfafce378a90a3250724d397
justin garcia <justinframedev@gmail.com>
2018-12-02 15:30:46 -0700

    nacigation and start menu page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 
-$(".dropdown-trigger").dropdown();
+$(".dropdown-trigger").dropdown(
+    {
+        coverTrigger: false,
+        hover: true
+    }
+);
 
 $(document).ready(function () {
     $('.sidenav').sidenav();
@@ -10,5 +15,36 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('.carousel').carousel();
+    $('.carousel').carousel(
+        {
+            fullWidth: true,
+            duration: 100,
+            indicators: true
+        }
+    );
+
 });
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    duration: 100,
+    indicators: true
+});
+
+
+$("a[href^='#']").click(function (e) {
+    e.preventDefault();
+    var page_url = window.location.href;
+    var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
+
+    $("body, html").animate({
+        scrollTop: $("#scroll-" + page_id).offset().top
+    }, 1000);
+});
+
+
+
+
+
+
+
