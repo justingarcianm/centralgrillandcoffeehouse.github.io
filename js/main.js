180d26a003e46e4bf37dce459f400448d2b08172
justin garcia <justinframedev@gmail.com>
2018-12-03 10:13:53 -0700

    reset sliders for home page

--- js/main.js
+++ js/main.js
@@ -1,50 +1,24 @@
-
-$(".dropdown-trigger").dropdown(
-    {
-        coverTrigger: false,
-        hover: true
-    }
-);
-
 $(document).ready(function () {
     $('.sidenav').sidenav();
-});
 
-$(document).ready(function () {
-    $('.parallax').parallax();
-});
-
-$(document).ready(function () {
-    $('.carousel').carousel(
+    $('.scrollspy').scrollSpy();
+    $('.carousel.carousel-slider').carousel(
         {
             fullWidth: true,
-            duration: 100,
             indicators: true
         }
     );
-
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    $(".dropdown-trigger").dropdown(
+        {
+            coverTrigger: false,
+            hover: true
+        }
+    );
 });
-
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    duration: 100,
-    indicators: true
-});
-
-
-$("a[href^='#']").click(function (e) {
-    e.preventDefault();
-    var page_url = window.location.href;
-    var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
-
-    $("body, html").animate({
-        scrollTop: $("#scroll-" + page_id).offset().top
-    }, 1000);
-});
-
-
-
-
 
 
 
